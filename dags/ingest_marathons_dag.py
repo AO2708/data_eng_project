c67b1f773ccce7ff4ec346263a1285bfa81686a0
AO2708 <a.pitardbouet@gmail.com>
2025-11-03 17:07:13 +0100

    🐛 Dag for Boston marathons

--- dags/ingest_marathons_dag.py
+++ dags/ingest_marathons_dag.py
@@ -8,6 +8,8 @@ from pymongo.errors import BulkWriteError
 
 START_DATE = pendulum.datetime(2025, 10, 20, tz="UTC")
 
+YEARS = [2019, 2018, 2017, 2016, 2015]
+
 with DAG(
     dag_id="ingest_marathons",
     start_date=START_DATE,
@@ -18,38 +20,43 @@ with DAG(
     tags=["ingestion"]
 ) as dag :
 
-    def insert_marathons_data(**context):
+    def insert_marathons_data(year):
         hook = MongoHook(conn_id='mongo_default')
         client = hook.get_conn()
         db = client['project']
-        collection = db['major_marathons']
+        collection = db['boston_marathons']
         collection.create_index("ID", unique=True)
 
-        df = pd.read_csv("/opt/airflow/data/major_marathons_data.csv")
+        df = pd.read_csv(f"/opt/airflow/data/{year}_marathons_data.csv", on_bad_lines='skip')
+        df["edition"] = year
+        df["ID"] = df["place_overall"].astype(str) + "_" + df["edition"].astype(str)
         data_to_insert = df.to_dict('records')
         try:
             collection.insert_many(data_to_insert, ordered=False)
-            print(f"{len(data_to_insert)} inserted documents into 'major_marathons' collection.")
+            print(f"{len(data_to_insert)} inserted documents into 'major_marathons' collection for {year} Boston marathon.")
         except BulkWriteError as bwe:
             write_errors = bwe.details.get("writeErrors", [])
             dup_count = sum(1 for err in write_errors if err.get("code") == 11000)
             total_inserts = len(data_to_insert) - dup_count
-            print(f"{dup_count} detected and ignored duplicates.")
-            print(f"{total_inserts} new inserted documents into 'major_marathons' collection.")
-        
+            print(f"{dup_count} detected and ignored duplicates for {year} Boston marathon.")
+            print(f"{total_inserts} new inserted documents into 'major_marathons' collection for {year} Boston marathon.")
+
         client.close()
 
-    get_spreadsheet = BashOperator(
-        task_id="get_spreadsheet",
-        bash_command=(
-            "curl -fsSL https://raw.githubusercontent.com/ali-ce/datasets/master/Marathon-Majors/Races.csv "
-            "--output /opt/airflow/data/major_marathons_data.csv"
+    for year in YEARS:
+
+        get_spreadsheet = BashOperator(
+            task_id=f"get_spreadsheet_{year}",
+            bash_command=(
+                f"curl -fsSL https://raw.githubusercontent.com/adrian3/Boston-Marathon-Data-Project/refs/heads/master/results{year}.csv "
+                f"--output /opt/airflow/data/{year}_marathons_data.csv"
+            )
         )
-    )
 
-    insert_marathons = PythonOperator(
-        task_id='insert_marathons_data',
-        python_callable=insert_marathons_data
-    )
+        insert_marathons = PythonOperator(
+            task_id=f'insert_marathons_data_{year}',
+            python_callable=insert_marathons_data,
+            op_kwargs={'year': year}
+        )
 
-    get_spreadsheet >> insert_marathons
+        get_spreadsheet >> insert_marathons
