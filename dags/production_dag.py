9762bb5f028a70101dce4e0fbbc6c270633adad1
AO2708 <a.pitardbouet@gmail.com>
2025-11-12 16:34:18 +0100

    ✨ create tables production stage

--- dags/production_dag.py
+++ dags/production_dag.py
@@ -23,6 +23,58 @@ with DAG(
     tags=["tp_04"],
 ) as dag:
     
+    def _create_tables_query(output_folder:str):
+        with open("/opt/airflow/data/create_tables.sql", "w") as f:
+            f.write(
+                "CREATE TABLE IF NOT EXISTS DimWeather(\n"
+                "   WeatherKey SERIAL PRIMARY KEY,\n"
+                "   AverageTemperature NUMERIC(3,1),\n"
+                "   Precipitation NUMERIC(3,1),\n"
+                "   Snow NUMERIC(3,1),\n"
+                "   WindSpeed NUMERIC(4,1),\n"
+                "   Pressure NUMERIC(5,1),\n"
+                "   SunTime NUMERIC(5,1),\n"
+                ");\n"
+            )
+            f.write(
+                "CREATE TABLE IF NOT EXISTS DimRunner(\n"
+                "   RunnerKey SERIAL PRIMARY KEY,\n"
+                "   Name VARCHAR(100),\n"
+                "   Age INT,\n"
+                "   Genre VARCHAR(1),\n"
+                "   Nationality VARCHAR(50)\n"
+                ");\n"
+            )
+            f.write(
+                "CREATE TABLE IF NOT EXISTS DimLocalisation(\n"
+                "   LocalisationKey SERIAL PRIMARY KEY,\n"
+                "   City VARCHAR(50)\n"
+                ");\n"
+            )
+            f.write(
+                "CREATE TABLE IF NOT EXISTS DimDate(\n"
+                "   DateKey SERIAL PRIMARY KEY,\n"
+                "   FullDate DATE NOT NULL,\n"
+                "   Year INT,\n"
+                "   Month INT,\n"
+                "   Day INT\n"
+                ");\n"
+            )
+            f.write(
+                "CREATE TABLE IF NOT EXISTS FactRaceResult(\n"
+                "   RaceResultId SERIAL PRIMARY KEY\n"
+                "   DateKey INT REFERENCES DimDate(DateKey),\n"
+                "   RaceKey INT REFERENCES DimRace(RaceKey),\n"
+                "   RunnerKey INT REFERENCES DimRunner(RunnerKey),\n"
+                "   WeatherKey INT REFERENCES DimWeather(WeatherKey),\n"
+                "   OverallRanking INT,\n"
+                "   GenderRanking INT,\n"
+                "   Time Time,\n"
+                "   Pace Time\n"
+                ");\n"
+            )
+            
+
     def _extract_runners_query(output_folder:str):
         with open("/opt/airflow/data/extract_runners.sql", "w") as f:
             f.write(
@@ -57,6 +109,21 @@ with DAG(
             f.write("\nON CONFLICT (RunnerKey) DO NOTHING;\n")
             f.write("SELECT setval('dimrunner_runnerkey_seq', COALESCE((SELECT MAX(RunnerKey) FROM DimRunner), 0),  true);\n")
 
+    create_tables_query = PythonOperator(
+        task_id="create_tables_query",
+        python_callable=_create_tables_query,
+        op_kwargs={
+            "output_folder": "/opt/airflow/data",
+        },
+    )
+
+    create_tables = SQLExecuteQueryOperator(
+        task_id="create_tables",
+        conn_id="OLTP",
+        sql="create_tables.sql",
+        autocommit=True,
+        handler=None,
+    )
 
     extract_runners_query = PythonOperator(
         task_id="extract_runners_query",
@@ -90,4 +157,5 @@ with DAG(
         handler=None,
     )
 
+    create_tables_query >> create_tables >> [extract_runners_query]
     extract_runners_query >> extract_runners >> insert_runners_query >> insert_runners
